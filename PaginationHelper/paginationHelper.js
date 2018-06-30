6d6806c88e8d69a089c700a7abd7297d8bc2500f
Ilya <shatuncheg@gmail.com>
2018-06-30 22:30:14 +0200

    Make PaginationHelper class

--- PaginationHelper/paginationHelper.js
+++ PaginationHelper/paginationHelper.js
@@ -1,4 +1,35 @@
 'use strict';
 
-// Use ES6 class syntax for this task
+function PaginationHelper(collection, items_per_page) {
+  this.collection = collection;
+  this.items_per_page = items_per_page;
 
+  function item_count() {
+    return this.collection.length;
+  }
+
+  function page_count() {
+    return Math.floor(this.collection.length / this.items_per_page) + 1;
+  }
+
+  function page_item_count(page_index) {
+    if (page_index >= 0 && page_index < (this.page_count() - 1)) {
+      return this.items_per_page;
+    }
+    else if (page_index === (this.page_count() - 1)) {
+      return this.item_count() % this.items_per_page
+    }
+    else {
+      return -1;
+    }
+  }
+
+  function page_index(item_index) {
+    if (item_index >= 0 && item_index < this.item_count()) {
+      return Math.floor(item_index / this.items_per_page);
+    }
+    return -1;
+  }
+}
+
+module.exports.PaginationHelper = PaginationHelper;
--- PaginationHelper/test.js
+++ PaginationHelper/test.js
@@ -1,9 +1,9 @@
-const { anagrams } = require('./anagrams');
+const { PaginationHelper } = require('./PaginationHelper');
 const test = require('tape');
 
-test('Find anagrams of word in array of words', function (t) {
-  t.deepEqual(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']), ['aabb', 'bbaa'], "Should be ['aabb', 'bbaa']");
-  t.deepEqual(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']), ['carer', 'racer'], "Should be ['carer', 'racer']");
-  t.deepEqual(anagrams('ahah', ['haha', 'asdasd', 'hhaa', 'aahh']), [ 'haha', 'hhaa', 'aahh' ], "Should be [ 'haha', 'hhaa', 'aahh' ]");
+test('Describe PaginationHelper class', function (t) {
+  let arr = new Array(25);
+  let helper = new PaginationHelper(arr, 10);
+  t.equal(helper.page_count(), 3, 'page_count is returning incorrect value.')
   t.end();
 });
