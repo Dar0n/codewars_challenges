38e8311ca4b13eeec3dda0c07ce3bbbe51a44504
Ilya <shatuncheg@gmail.com>
2018-09-28 21:06:59 +0200

    Make function to build diamond of odd width with asterisks

--- give_me_diamond/diamond.js
+++ give_me_diamond/diamond.js
@@ -1,6 +1,16 @@
 const diamond = (n) => {
-  console.log(n);
-  return n;
+  if (n <= 0 || (n % 2 === 0)) {
+    return null;
+  }
+  let result = '*'.repeat(n) + '\n';
+  let spaces = 1;
+  while (n > 1) {
+    n -= 2;
+    result = ' '.repeat(spaces) + '*'.repeat(n) + '\n' + result +  ' '.repeat(spaces) + '*'.repeat(n) + '\n';
+    spaces ++;
+  }
+  console.log(result);
+  return result;
 }
 
 module.exports.diamond = diamond;
--- give_me_diamond/diamond_test.js
+++ give_me_diamond/diamond_test.js
@@ -2,6 +2,10 @@ const test = require('tape');
 const { diamond } = require('./diamond');
 
 test('If possible, draw a diamon of given size', function(t){
-  t.equal(diamond(2), 2, 'Should be just equal parameter');
+  t.equal(diamond(3), " *\n***\n *\n");
+  t.equal(diamond(5), "  *\n ***\n*****\n ***\n  *\n");
+  t.equal(diamond(2), null);
+  t.equal(diamond(-3), null);
+  t.equal(diamond(0), null);
   t.end();
 })
