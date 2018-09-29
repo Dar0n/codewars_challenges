ed38a41620171161d10f989b29adfb0788626046
Ilya <shatuncheg@gmail.com>
2018-09-29 20:03:36 +0200

    Solution works. Its not effective enough, but it works.

--- sum_of_pairs/sum_pairs.js
+++ sum_of_pairs/sum_pairs.js
@@ -1,8 +1,17 @@
 function sum_pairs(ints, n) {
   let result = [];
-  for (let i = 0; i < ints.length; i++) {
-    
+  for (let i = 0; i < ints.length - 1; i++){
+    const diff = n - ints[i];
+    for (let j = i + 1; j < ints.length; j++) {
+      if (ints[j] === diff) {
+        result.push([i, j]);
+      }
+    }
   }
+  if (!result.length) return undefined;
+  result.sort((arr1, arr2) => arr1[1] - arr2[1]);
+  const [first, second] = result[0];
+  return [ints[first], ints[second]];
 }
 
 module.exports.sum_pairs = sum_pairs;
--- sum_of_pairs/test_sum_pairs.js
+++ sum_of_pairs/test_sum_pairs.js
@@ -2,6 +2,7 @@ const test = require('tape');
 const { sum_pairs } = require('./sum_pairs');
 
 test('Sum pairs in array to certain number, return first pair', function(t){
-  t.equal(sum_pairs([1, 2, 3, 4, 5], 6), [2, 4]);
+  t.deepEqual(sum_pairs([1, 2, 3, 4, 5], 6), [2, 4]);
+  t.deepEqual(sum_pairs([7, 2, -1, 4, 5], 6), [7, -1]);
   t.end();
 })
