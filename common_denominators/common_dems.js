3ffe65eae281873112a7ab8a82581e3144a15a04
Ilya <shatuncheg@gmail.com>
2018-10-01 11:53:24 +0200

    Make a function to find lowest common multiplier for array of pairs of numbers. Return modified array with LCM as second argument.

--- common_denominators/common_dems.js
+++ common_denominators/common_dems.js
@@ -1,5 +1,17 @@
 const common_dems = (arr) => {
-  return arr;
+  const myLCM =  arr.reduce((result, el) => lcm(result, el[1]), 1);
+  return arr.map(el => [myLCM / el[1], myLCM]).toString();
+}
+
+const gcd = (a, b) => {
+  if (b === 0) return a;
+  else {
+    return gcd(b, a % b);
+  }
+}
+
+const lcm = (a, b) => {
+  return a * b / gcd(a, b);
 }
 
 module.exports.common_dems = common_dems;
