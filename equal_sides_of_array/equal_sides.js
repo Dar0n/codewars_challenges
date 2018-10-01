0bbee93339463d64a4b500f18aac110fd19ce698
Ilya <shatuncheg@gmail.com>
2018-10-01 10:45:34 +0200

    Make a function to keep sum to the left and to the right of the current element. Return index if sums are equal.

--- equal_sides_of_array/equal_sides.js
+++ equal_sides_of_array/equal_sides.js
@@ -1,11 +1,19 @@
 const equal_sides = (arr) => {
-  for (const el, i of arr) {
-    console.log(el, i);
+  if (!arr.length) {
+    return -1;
   }
-}
-
-const sum = (arr) => {
-  return arr.reduce((a,b) => a+b, 0);
+  //[1,2,3,2,1]
+  let sum = arr.reduce((sum, el) => sum + el, 0);
+  let left_sum = 0;
+  let right_sum = sum;
+  for (let i = 0; i < arr.length; i++) {
+    right_sum -= arr[i];
+    if (left_sum === right_sum) {
+      return i;
+    }
+    left_sum += arr[i];
+  }
+  return -1;
 }
 
 module.exports.equal_sides = equal_sides;
--- equal_sides_of_array/test_equal_sides.js
+++ equal_sides_of_array/test_equal_sides.js
@@ -1,7 +1,10 @@
 const test = require('tape');
-const equal_sides = require('./equal_sides');
+const { equal_sides } = require('./equal_sides');
 
 test('Find element in array with equal sum sides', function(t){
   t.equal(equal_sides([1,2,3,2,1]), 2);
+  t.equal(equal_sides([1,2,-1,-2,10]), 4);
+  t.equal(equal_sides([]), -1);
+  t.equal(equal_sides([1,2,3,4,5]), -1);
   t.end();
 })
