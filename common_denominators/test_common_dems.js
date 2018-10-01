e5c3b5efd50d96dec84c5e9a534da2b2a67651c3
Ilya <shatuncheg@gmail.com>
2018-10-01 11:56:35 +0200

    Change output to a string with required formatting.

--- common_denominators/test_common_dems.js
+++ common_denominators/test_common_dems.js
@@ -2,6 +2,6 @@ const test = require('tape');
 const { common_dems } = require('./common_dems');
 
 test('Find common denominators in paris', function(t){
-  t.equal(common_dems([ [1, 2], [1, 3], [1, 4] ]), "6,12,4,12,3,12");
+  t.equal(common_dems([ [1, 2], [1, 3], [1, 4] ]), "(6,12)(4,12)(3,12)");
   t.end();
 })
