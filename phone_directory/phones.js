7962e74a2127e84fe6d43b7642ae1e7d134d86fb
Ilya <shatuncheg@gmail.com>
2018-10-09 15:25:12 +0200

    Make a function to look for a number in the phonebook and return the match if there is only one.

--- phone_directory/phones.js
+++ phone_directory/phones.js
@@ -1,5 +1,33 @@
 function phones(str, num) {
-  return num;
+  let lines = str.split('\n');
+  if (lines[lines.length-1] === '') lines.pop();
+  let phonebook = [];
+  lines.forEach((line, i) => {
+    let numberRegex = /\+\d{1,2}-\d{3}-\d{3}-\d{4}/;
+    let nameRegex = /<(\w+)>/;
+    let trailingSpaces = /^\s+|\s+$/;
+    let number = line.match(numberRegex);
+    let name = line.match(nameRegex);
+    let address = line.replace(numberRegex, '').replace(nameRegex, '').replace(trailingSpaces, '');
+    if (name && number && address) {
+      phonebook.push({
+        name: name[0].substr(1, name[0].length-2),
+        number: number[0],
+        address: address,
+      })
+    }
+  })
+  let result = phonebook.filter(note => note.number === num);
+  switch (result.length){
+    case 0:
+      return `Error => Not found: ${num}`;
+    case 1:
+      return `Phone => ${result[0].number}, Name => ${result[0].name}, Address => ${result[0].address}`;
+    default:
+      return `Error => Too many people: ${num}`;
+  }
 }
+
+
 
 module.exports.phones = phones;
--- phone_directory/test_phones.js
+++ phone_directory/test_phones.js
@@ -2,6 +2,14 @@ const { phones } = require('./phones');
 const test = require('tape');
 
 test('Lets find some numbers', function(t){
-  t.equal();
+  t.equal(phones(`<John> +1-453-453-1234 trhdthty\n
+    rt +23-342-534-5345 <Rehanna> gretg\n
+    f+234-234-2342werr\nerfref\n`, '+1-453-453-1234'), 'Phone => +1-453-453-1234, Name => John, Address => trhdthty', 'Found the name');
+    t.equal(phones(`<John> +1-453-453-1234 trhdthty\n
+    rt +23-342-534-5345 <Rehanna> gretg\n
+    f+234-234-2342werr\nerfref\n`, '+1-453-453-123'), `Error => Not found: +1-453-453-123`, 'No match for the number');
+    t.equal(phones(`<John> +1-453-453-1234 trhdthty\n
+    rt +1-453-453-1234 <Rehanna> gretg\n
+    f+234-234-2342werr\nerfref\n`, '+1-453-453-1234'), `Error => Too many people: +1-453-453-1234`, 'More than one found');
   t.end();
 })
